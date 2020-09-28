ec676fc0c64aa88d1e726913ee86fa9563b79491
Linusrbk <linus.astrom@elev.ga.ntig.se>
2020-09-28 13:23:17 +0200

    Update 2,4.py

--- 2,4.py
+++ 2,4.py
@@ -1,6 +1,7 @@
-miles = float(input('hur många miles har din bil åkt?'))
-gallon = float(input('hur många gallons av bensin har din bil använt? '))
-km = miles * 1.609
-liter = gallon * 3.785
-print('din bil har åkt', km ,'km')
-print('din bil har använt', liter ,'liter')
+#programmet ska konvertera USAs miles/gallons till km/liter
+miles = float(input('hur många miles har din bil åkt?')) # konverterar mumret man skrivet ut till datatypen float 
+gallon = float(input('hur många gallons av bensin har din bil använt? ')) #samma som 2an
+km = miles * 1.609 #tar värdet som getts om miles och omvandlar det till km
+liter = gallon * 3.785 #samma som 4n fast men gallon och liter
+print(f'din bil har åkt {km:.2f} km') #printar ut det slutliga värdet med en text som beskriver att det är i km nu 
+print(f'din bil har använt {liter:.2f}liter')#samma som 6an men med liter
